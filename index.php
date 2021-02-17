b1c8f3fc6df022a9b88fc21dfd53150938902798
cemeris <4vitalik4@gmail.com>
2021-02-17 21:18:56 +0200

    changes

--- index.php
+++ index.php
@@ -3,19 +3,25 @@
 
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     include 'DB.php';
     $game = new DB('game');
 ?>
-<div class="container">
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
 
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
+<?php if(array_key_exists('game-id', $_REQUEST)) { ?>
+    <div class="container">
 
-    <a href="#"></a>
-    <a href="#"></a>
-    <a href="#"></a>
-</div>
+    <?php for ($r = 1; $r <= 3; $r++):?>
+        <?php for ($c = 1; $c <= 3; $c++):?>
+            <a href="?c=<?=$c?>&r=<?=$r?>">-</a>
+        <?php endfor; ?>
+    <?php endfor; ?>
+
+    </div>
+
+<?php }else { ?>
+    <a class="btn" href="startgame.php">start game</a>
+<?php } ?>
